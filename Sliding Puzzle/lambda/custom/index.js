c1a229790096d8a75fd151475461068e9a2dad26
Alex Xiong <veralexxiong@outlook.com>
2018-02-14 22:15:53 +0800

    new skill deployed, local test simulator works

--- Sliding Puzzle/lambda/custom/index.js
+++ Sliding Puzzle/lambda/custom/index.js
@@ -2,18 +2,18 @@ const Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
-    alexa.appId = "amzn1.ask.skill.930877b6-88a6-43ed-86d1-b4b8fbfeefd8"; // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
+    alexa.appId = "amzn1.ask.skill.366897d9-f60f-4e9d-83cb-a731c5855e6f"; // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 const handlers = {
 	'LaunchRequest': function(){
-		this.emit('InitializeIntent');
+		this.emit(':tell',"Welcome to JigsawPuzzle!");
 	},
 
 	'InitializeIntent': function() {
 		// emit response
-		this.emit(':tell', 'Welcome to JigsawPuzzle!')
+		this.emit(':tell', 'Enjoy the game!')
 	}
 };
